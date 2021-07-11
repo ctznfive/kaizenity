ca2ffea10eee87480638aa1da9fb372d23cdfc23
ctznfive <ctznfive@gmail.com>
2021-07-11 15:44:39 +0300

    Refactoring

--- kaizenity.go
+++ kaizenity.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "sort"
-    "encoding/json"
-    "github.com/rivo/tview"
-    "github.com/gdamore/tcell/v2"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 const (
-    AppName = "Kaizenity"
-    Version = "1.0.0"
-    Hotkeys = "[i] Add  [D] Remove  [h j k l] Select  [H J K L] Move  [q] Quit"
-    ColorElem = tcell.ColorBlue
-    DBName = "kaizenitydb.json"
+	AppName   = "Kaizenity"
+	Version   = "1.0.0"
+	DBName    = "kaizenitydb.json"
+	Hotkeys   = "[i] Add  [D] Remove  [h j k l] Select  [H J K L] Move  [q] Quit"
+	ColorElem = tcell.ColorBlue
 )
 
 var (
-    cards    Cards
-
-    app = tview.NewApplication()
+	cards Cards
+	app   = tview.NewApplication()
 )
 
 type Card struct {
-    ID     int64  `json:"id"`
-    Name   string `json:"name"`
-    Desc   string `json:"desc"`
-    Column int    `json:"column"`
-    Pos    int64  `json:"pos"`
+	ID     int64  `json:"id"`
+	Name   string `json:"name"`
+	Desc   string `json:"desc"`
+	Column int    `json:"column"`
+	Pos    int64  `json:"pos"`
 }
 
 // Cards implements sort.Interface for []Card based on the Pos field
@@ -40,113 +40,103 @@ func (c Cards) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 func (c Cards) Less(i, j int) bool { return c[i].Pos < c[j].Pos }
 
 func (c *Cards) ReadCards(path string) error {
-    path += DBName
-    jsonBlob, err := os.ReadFile(path)
-    if err != nil {
-        return err
-    }
-    err = json.Unmarshal(jsonBlob, c)
-    return err
+	path += DBName
+	jsonBlob, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(jsonBlob, c)
+	return err
 }
 
 func (c *Cards) DrawCards(column int, primitive tview.Primitive) {
-    primitive.(*tview.List).Clear()
-    for _, card := range *c {
-        if card.Column == column {
-            primitive.(*tview.List).AddItem(card.Name, card.Desc, 0, nil)
-        }
-    }
+	primitive.(*tview.List).Clear()
+	for _, card := range *c {
+		if card.Column == column {
+			primitive.(*tview.List).AddItem(card.Name, card.Desc, 0, nil)
+		}
+	}
 }
 
 func eventInput(event *tcell.EventKey) *tcell.EventKey {
-    switch event.Rune() {
-    case 'q':
-        app.Stop()
-    case 'j':
-        idxCurrent := app.GetFocus().(*tview.List).GetCurrentItem()
-        app.GetFocus().(*tview.List).SetCurrentItem(idxCurrent + 1)
-    case 'k':
-        idxCurrent := app.GetFocus().(*tview.List).GetCurrentItem()
-        if idxCurrent > 0 {
-            app.GetFocus().(*tview.List).SetCurrentItem(idxCurrent - 1)
-        }
-    }
-    return event
+	switch event.Rune() {
+	case 'q':
+		app.Stop()
+	case 'j':
+		idxCurrent := app.GetFocus().(*tview.List).GetCurrentItem()
+		app.GetFocus().(*tview.List).SetCurrentItem(idxCurrent + 1)
+	case 'k':
+		idxCurrent := app.GetFocus().(*tview.List).GetCurrentItem()
+		if idxCurrent > 0 {
+			app.GetFocus().(*tview.List).SetCurrentItem(idxCurrent - 1)
+		}
+	}
+	return event
 }
 
-func mainDraw(columns []string, path string) error {
-    newPrimitive := func(text string) tview.Primitive {
-        return tview.NewTextView().
-            SetTextAlign(tview.AlignCenter).
-            SetTextColor(ColorElem).
-            SetText(text)
-        }
+func mainDraw(columnsStr []string, columnDefault int, path string) error {
+	var numColumns = len(columnsStr)
 
-    if err := cards.ReadCards(path); err != nil {
-        return err
-    }
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetTextColor(ColorElem).
+			SetText(text)
+	}
 
-    headerA := newPrimitive(columns[0])
-    headerB := newPrimitive(columns[1])
-    headerC := newPrimitive(columns[2])
-    headerD := newPrimitive(columns[3])
+	if err := cards.ReadCards(path); err != nil {
+		return err
+	}
 
-    columnA := tview.NewList()
-    columnB := tview.NewList()
-    columnC := tview.NewList()
-    columnD := tview.NewList()
+	grid := tview.NewGrid().
+		SetRows(1, -1, 1).
+		SetBorders(true)
 
-    footer := newPrimitive("[ " + AppName + " " + Version + " ]  " + Hotkeys)
+	headers := make([]tview.Primitive, 0)
+	columns := make([]tview.Primitive, 0)
+	for i := 0; i < numColumns; i++ {
+		headers = append(headers, newPrimitive(columnsStr[i]))
+		columns = append(columns, tview.NewList().SetSelectedFocusOnly(true))
 
-    grid := tview.NewGrid().
-        SetRows(1, -1, 1).
-        SetColumns(-1, -1, -1, -1).
-        SetBorders(true)
+		// Layout for screens narrower than 100 cells (only default column is visible)
+		if i != columnDefault {
+			grid.AddItem(headers[i], 0, 0, 0, 0, 0, 0, false).
+				AddItem(columns[i], 0, 0, 0, 0, 0, 0, false)
+		} else {
+			grid.AddItem(headers[i], 0, 0, 1, numColumns, 0, 0, false).
+				AddItem(columns[i], 1, 0, 1, numColumns, 0, 0, false)
+		}
 
-	// Layout for screens narrower than 100 cells (only TODO is visible)
-    grid.AddItem(headerA, 0, 0, 0, 0, 0, 0, false).
-         AddItem(columnA, 0, 0, 0, 0, 0, 0, false).
-         AddItem(headerB, 0, 0, 0, 0, 0, 0, false).
-         AddItem(columnB, 0, 0, 0, 0, 0, 0, false).
-         AddItem(headerC, 0, 0, 1, 4, 0, 0, false).
-         AddItem(columnC, 1, 0, 1, 4, 0, 0, true).
-         AddItem(headerD, 0, 0, 0, 0, 0, 0, false).
-         AddItem(columnD, 0, 0, 0, 0, 0, 0, false).
-         AddItem(footer, 2, 0, 1, 4, 0, 0, false)
+		// Layout for screens wider than 100 cells
+		grid.AddItem(headers[i], 0, i, 1, 1, 0, 100, false).
+			AddItem(columns[i], 1, i, 1, 1, 0, 100, false)
+	}
 
-	// Layout for screens wider than 100 cells
-    grid.AddItem(headerA, 0, 0, 1, 1, 0, 100, false).
-         AddItem(columnA, 1, 0, 1, 1, 0, 100, true).
-         AddItem(headerB, 0, 1, 1, 1, 0, 100, false).
-         AddItem(columnB, 1, 1, 1, 1, 0, 100, false).
-         AddItem(headerC, 0, 2, 1, 1, 0, 100, false).
-         AddItem(columnC, 1, 2, 1, 1, 0, 100, false).
-         AddItem(headerD, 0, 3, 1, 1, 0, 100, false).
-         AddItem(columnD, 1, 3, 1, 1, 0, 100, false).
-         AddItem(footer, 2, 0, 1, 4, 0, 0, false)
+	footer := newPrimitive("[ " + AppName + " " + Version + " ]  " + Hotkeys)
+	grid.AddItem(footer, 2, 0, 1, numColumns, 0, 0, false)
 
-    sort.Sort(Cards(cards))
-    cards.DrawCards(0, columnA)
-    cards.DrawCards(1, columnB)
-    cards.DrawCards(2, columnC)
-    cards.DrawCards(3, columnD)
+	sort.Sort(Cards(cards))
+	for i := 0; i < numColumns; i++ {
+		cards.DrawCards(i, columns[i])
+	}
 
-    app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-        return eventInput(event)
-    })
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		return eventInput(event)
+	})
 
-    if err := app.SetRoot(grid, true).EnableMouse(true).SetFocus(columnA).Run(); err != nil {
-        log.Fatal(err)
-    }
+	if err := app.SetRoot(grid, true).EnableMouse(true).SetFocus(columns[columnDefault]).Run(); err != nil {
+		log.Fatal(err)
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    columnsFlow := []string{"BACKLOG", "TODO", "DOING", "DONE"}
-    pathInit := ""
+	columnsStr := []string{"BACKLOG", "TODO", "DOING", "DONE"}
+	columnDefault := 0
+	pathInit := ""
 
-    if err := mainDraw(columnsFlow, pathInit); err != nil {
-        fmt.Println(err)
-    }
+	if err := mainDraw(columnsStr, columnDefault, pathInit); err != nil {
+		fmt.Println(err)
+	}
 }
